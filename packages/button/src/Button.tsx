ac88776c3e907594e9089d19e0c269f64712bede
programmingwithnavilla <navidbarsalari@gmail.com>
2025-06-26 13:00:29 +0330

    add property for button

--- packages/button/src/Button.tsx
+++ packages/button/src/Button.tsx
@@ -2,13 +2,35 @@ import React from "react";
 import { ButtonProps } from "./Button.type";
 
 export const Button = ({
+  id,
+  name,
+  className,
   children,
   onClick,
+  onBlur,
+  autoFocus,
   disabled = false,
   type = "button",
+  value,
+  popoverTarget,
+  popoverTargetAction,
+  ariaPressed,
 }: ButtonProps) => {
   return (
-    <button onClick={onClick} disabled={disabled} type={type}>
+    <button
+      id={id}
+      name={name}
+      className={className}
+      disabled={disabled}
+      type={type}
+      value={value}
+      popoverTarget={popoverTarget}
+      popoverTargetAction={popoverTargetAction}
+      autoFocus={autoFocus}
+      onClick={onClick}
+      onBlur={onBlur}
+      aria-pressed={ariaPressed}
+    >
       {children}
     </button>
   );
